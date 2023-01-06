96f3145604a443c305cabc6d78d0c1c513ff2903
szymonpolitowski <szymon.politowski.96@gmail.com>
2023-01-06 22:45:02 +0100

    dodano deklaracje wiadomosci

--- Code/Messenger/messenger_declarations.h
+++ Code/Messenger/messenger_declarations.h
@@ -1,8 +1,15 @@
 #ifndef MESSENGER_DECLARATIONS_H
 #define MESSENGER_DECLARATIONS_H
+#include <stdint.h>
 
 
-typedef enum {
+const int c_MaxTextMsgLen   = 128;  // Maksymalna liczba znakow w wiadomosci (?tekstowej)
+
+const int c_MaxPacketSize   = 9600; // Maksymalny rozmiar wysylanej paczki
+
+
+typedef enum
+{
 
     statusInactive      = 0,    // Nieaktywny
     statusActive        = 1,    // Aktywny
@@ -11,7 +18,63 @@ typedef enum {
 
 } UserStatus_e;
 
+typedef enum
+{
 
+    msgType_Text        = 0,
+    msgType_Picture     = 1,
+    msgType_NotDefined  = 2,
+    msgType_Status      = 3
+
+}MsgType_e;
+
+typedef struct
+{
+    uint32_t msgType;           // Rodzaj wiadomosci
+    uint32_t addressRx;         // Numer odbiorcy
+    uint32_t addressTx;         // Numer nadawcy
+    uint64_t timeUTC;           // Czas UTC (sekundy od epoki Linux)
+    uint16_t packetSize;        // Rozmiar pakietu
+    uint8_t reserved;           // RESERVED - do not use
+
+} __attribute__((packed)) MsgHeader_s;
+
+typedef struct
+{
+    uint8_t len;                // Dlugosc wiadomosci
+    char msg[c_MaxTextMsgLen];  // Wiadomosc tekstowa
+
+} __attribute__((packed)) MsgText_s;
+
+typedef struct
+{
+    uint8_t picture;            // Numer obrazka
+
+} __attribute__((packed)) MsgPicture_s;
+
+typedef struct
+{
+    UserStatus_e status;        // Status uzytkownika
+
+} __attribute__((packed)) MsgStatus_s;
+
+typedef struct
+{
+    MsgHeader_s header;
+    MsgText_s msg;
+} __attribute__((packed)) PacketMsgText_s;
+
+typedef struct
+{
+    MsgHeader_s header;
+    MsgPicture_s image;
+} __attribute__((packed)) PacketImgText_s;
+
+typedef struct
+{
+    MsgHeader_s header;
+    MsgStatus_s status;
+} __attribute__((packed)) PacketStatusText_s;
 
 
 #endif // MESSENGER_DECLARATIONS_H
